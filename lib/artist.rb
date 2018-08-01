58e9c7d1ab24602c68b36c729b38e5b882c9ec87
Koy Saeteurn <kosatun@aol.com>
2018-08-01 19:47:48 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,15 @@
+class Artist 
+  attr_accessor :name 
+  
+  def initialize 
+    @name = name 
+    @songs = []
+  end 
+  
+  def add_song(song)
+    @songs << song 
+    song.artist = self 
+  end   
+  
+  def add_song_by_name(song)
+    song = Song.new()
