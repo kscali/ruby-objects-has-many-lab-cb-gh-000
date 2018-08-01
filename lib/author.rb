8d67c09bb8f299c0055a0bd6ec18892952efb499
Koy Saeteurn <kosatun@aol.com>
2018-08-01 20:39:27 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -3,27 +3,29 @@ class Author
   
   @@all = []
   
-  def initialize(name)
-    @name = name 
-    @@all << self 
-  end   
-  
-  def posts 
-    @@all 
-  end 
-  
-  def add_post(post)
-    post.author = self 
-  end   
-  
-  def add_post_by_title(post_title)
-    post = Post.new(post_title)
-    post.author = self 
+ def initialize(name)
+    @name = name
+    @@all << self
   end
-  
+
+  def self.all
+    @@all
+  end
+
+  def add_post(post)
+    post.author = self
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    post.author = self
+  end
+
+  def posts
+    Post.all.select {|post| post.author == self}
+  end
+
   def self.post_count
     Post.all.count
-  end   
-  
-end   
-  
+  end
+end
